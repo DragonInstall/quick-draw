91cd3ab0c20efdfe05d83e8de8aab5a06b6d1584
DragonInstall <lidortemno@gmail.com>
2026-06-06 13:43:49 +0300

    working

--- src/ui/mainUI.py
+++ src/ui/mainUI.py
@@ -2,7 +2,7 @@ import os
 import random
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 
 root=tk.Tk()
 root.title("Quick Draw")
@@ -12,17 +12,28 @@ app_width = int(screen_width * (2/3))
 app_height = screen_height
 root.geometry(f"{app_width}x{app_height}")
 root.lift()
+central_frame = tk.Frame(root)
+central_frame.pack(side="top", pady=10)
 
+INDEX=0
+LOOP_ID=None
+FONT_SIZE=14
+FONT_TYPE= "Arial"
+SESSION_RUNNING=False
+FOLDER_PATH= ""
+IMAGE_TIME=10
+IMAGE_AMOUNT=1
+FOLDER_IMAGES=[]
+
+#global labels
 image_time_clicked= tk.StringVar()
 image_num_clicked= tk.StringVar()
-folder_path=""
-image_time=10
-image_amount=1
-#global labels (refreshing ones)
-current_image_label=tk.Label()
-which_image_label=tk.Label()
+current_image_label=tk.Label(root)
+which_image_label=tk.Label(central_frame)
 timer_label=tk.Label()
-index=0
+folder_label=tk.Label()
+end_session_button=tk.Button()
+
 
 image_time_dict={"10 seconds": 10,
                  "30 seconds": 30,
@@ -47,16 +58,24 @@ image_num_dict={
     "120 images":120,
 }
 def get_folder():
-    global folder_path
-    folder_path = filedialog.askdirectory(title="Choose a folder")
+    global FOLDER_PATH,folder_label
+    FOLDER_PATH = filedialog.askdirectory(title="Choose a folder")
+    folder_label.config(text=f"{FOLDER_PATH}")
 
 def pre_session_ui():
-    global image_time_dict,image_num_dict,index
+    global image_time_dict,image_num_dict,INDEX,folder_label,LOOP_ID,SESSION_RUNNING
+    SESSION_RUNNING=False
+    for widget in root.winfo_children():
+        widget.destroy()
+
     #reset index for new session
-    index=0
+    INDEX=0
     #choose a file path
-    browse_button = tk.Button(root, text="Browse", command=get_folder)
-    browse_button.pack()
+    browse_button = tk.Button(root, text="Browse", command=get_folder, font=(FONT_TYPE, FONT_SIZE))
+    browse_button.pack(pady=10)
+
+    folder_label = tk.Label(root, text=f"{FOLDER_PATH}", font=(FONT_TYPE, FONT_SIZE))
+    folder_label.pack()
 
     #choose view time per image
     image_time_clicked.set(list(image_time_dict.keys())[0])
@@ -69,77 +88,126 @@ def pre_session_ui():
     image_num_dropdown.pack()
 
     #start session
-    start_session_button = tk.Button(root, text="Start Session", command=requirement_check)
+    start_session_button = tk.Button(root, text="Start Session", command=requirement_check, font=(FONT_TYPE, FONT_SIZE))
     start_session_button.pack()
 
 
 def requirement_check():
-    global folder_path,image_time,image_amount
-    if folder_path == "":
+    global FOLDER_PATH,IMAGE_TIME,IMAGE_AMOUNT,FOLDER_IMAGES,SESSION_RUNNING
+    if FOLDER_PATH == "":
         messagebox.showerror("Error", "Please select a folder")
         return
     #check if there are any images inside the file
     try:
-        all_files=os.listdir(folder_path)
-        image_files = [os.path.join(folder_path,f) for f in all_files if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+        all_files=os.listdir(FOLDER_PATH)
+        image_files = [os.path.join(FOLDER_PATH, f) for f in all_files if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
         if len(image_files) == 0:
             messagebox.showerror("Error", "No images found")
             return
         else:
             random.shuffle(image_files)
-            image_amount=image_num_dict[image_num_clicked.get()]
-            image_time=image_time_dict[image_time_clicked.get()]
+            IMAGE_AMOUNT=image_num_dict[image_num_clicked.get()]
+            IMAGE_TIME=image_time_dict[image_time_clicked.get()]
             #loops through the shuffled deck and back to 0 if not enough images
-            session_images = [image_files[i % len(image_files)] for i in range(image_amount)]
+            FOLDER_IMAGES = [image_files[i % len(image_files)] for i in range(IMAGE_AMOUNT)]
 
     except Exception as e:
         messagebox.showerror("Error", f"Could not read the folder:\n{e}")
         return
     #all good start session
-    session_ui(session_images)
+    SESSION_RUNNING=True
+    session_ui()
 
 
-def session_ui(session_images):
+def session_ui():
+    global which_image_label,current_image_label,central_frame,timer_label,end_session_button
     for widget in root.winfo_children():
         widget.destroy()
     root.update()
-    timer()
-    #loop through images until you done
-    image_loop(session_images)
+
+    #build frames
+    central_frame=tk.Frame(root)
+    central_frame.pack(side="top", pady=10)
+
+    forward_button=tk.Button(central_frame, text=">>", command=image_loop, font=(FONT_TYPE, FONT_SIZE))
+    forward_button.pack(side="right",padx=10)
+
+    backward_button=tk.Button(central_frame, text="<<", command=backward, font=(FONT_TYPE, FONT_SIZE))
+    backward_button.pack(side="left",padx=10)
+
+    which_image_label = tk.Label(central_frame, font=(FONT_TYPE, FONT_SIZE))
+    which_image_label.pack(side="top", pady=5)
+
+    current_image_label = tk.Label(root)
+    current_image_label.pack(expand=True)
+
+    end_session_button=tk.Button(root, text="End Session", command=pre_session_ui, font=(FONT_TYPE, FONT_SIZE))
+    end_session_button.place(relx=0.0,rely=0.0,anchor="nw",x=20,y=13)
 
 
+    timer_label=tk.Label(root,font=(FONT_TYPE, FONT_SIZE + 2))
+    timer_label.place(relx=1.0,rely=0.0,anchor="ne",x=-20,y=15)
+    img_time_copy=IMAGE_TIME-1
+    timer(img_time_copy)
+    image_loop()
 
 
-def image_loop(session_images):
-    global index,image_time,current_image_label,which_image_label
-
-    current_image_label.destroy()
-    which_image_label.destroy()
-    root.update()
-
-    max_width = root.winfo_width() - 20
-    max_height = root.winfo_height() - 20
-    if index>=len(session_images):
-        messagebox.showinfo("Completion","Session has ended")
+def image_loop():
+    global INDEX,IMAGE_TIME,current_image_label,which_image_label,FOLDER_IMAGES,LOOP_ID
+    delay= IMAGE_TIME * 1000
+    if not SESSION_RUNNING:
         return
-    else:
-        which_image_label=tk.Label(root,text=f"{index+1}/{len(session_images)}")
-        which_image_label.pack(side="top",pady=20)
+    if LOOP_ID is not None:
+        root.after_cancel(LOOP_ID)
+    root.update_idletasks()
 
-        opened_image = Image.open(session_images[index])
+    max_width = root.winfo_width()-20
+    max_height = root.winfo_height()-70
+
+    if INDEX>=len(FOLDER_IMAGES):
+        messagebox.showinfo("Completion","Session has ended")
+        pre_session_ui()
+        return
+
+    else:
+        which_image_label.config(text=f"{INDEX + 1}/{len(FOLDER_IMAGES)}")
+
+        opened_image = Image.open(FOLDER_IMAGES[INDEX])
         opened_image.thumbnail((max_width, max_height))
 
-        displayed_image = ImageTk.PhotoImage(image=opened_image)
+        displayed_image = ImageTk.PhotoImage(image=ImageOps.contain(opened_image,(max_width,max_height)))
 
-        current_image_label = tk.Label(root, image=displayed_image)
+        current_image_label .config( image=displayed_image)
         current_image_label.image = displayed_image
-        current_image_label.pack(expand=True)
-        index+=1
-        delay=image_time*1000
-        root.after(delay, image_loop, session_images)
+        INDEX+=1
+        LOOP_ID=root.after(delay, image_loop)
 
-def timer():
-    global image_time,timer_label
+def backward():
+    global INDEX
+    if INDEX>=2:
+        INDEX-=2
+        image_loop()
+    else:
+        return
+
+def forward():
+
+
+def timer(time):
+    global timer_label
+
+    if SESSION_RUNNING:
+        minutes = int(time / 60)
+        seconds = time % 60
+        timer_label.config(text=f"{minutes:02}:{seconds:02}")
+
+        if time>0:
+            time-=1
+        else:
+            time=IMAGE_TIME-1
+        root.after(1000, timer,time)
+    else:
+        return
 
 
 pre_session_ui()
